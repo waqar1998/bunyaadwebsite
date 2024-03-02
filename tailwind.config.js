e8c179505fd0d3ce09410006b06b34691c52bed8
WaqarKhan <62086493+waqar1998@users.noreply.github.com>
2024-03-02 08:11:24 +0500

    Add files via upload
    upload changes

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat']
+      },
+    },
+    colors: {
+      'main-gray': '#535353',
+      'hover':'#a8a9ad',
+      'main-5':'#E2E3E366',
+      'blue-900':'#1890FF',
+      'blue1':'#00b2ee',
+      'main6':'#F0F0F0',
+      'card-bg':'#F3F3F3',
+      'yellow':'#D48806',
+      'work-bg':'#EEEEEF',
+      'main-black':'#A8A9AD',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white' : '#fff',
+      'red': '#ff0000 ',
+      'black' :'#000',
+      'dark-orange':'#FF8C00',
+       },
+  },
+  plugins: [],
+}
